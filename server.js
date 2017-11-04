6e67276b7d5172f39212bb184b1a0b6d0fdf9dd4
me <me@mymac.com>
2017-11-03 21:04:52 -0700

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 //tell express we want to use partials
@@ -91,4 +92,7 @@ app.get('/bad', (req, res) => {
 
 //bind app to a port
 
-app.listen(3000);
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
+
+});
